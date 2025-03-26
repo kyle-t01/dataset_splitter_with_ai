91449dfd5bb19161fec49c066f7260b1ad4bbdad
kylet-01 <userEmail>
2025-03-26 19:15:59 +1100

    prompt box now visible in app homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+// components
+import PromptBox from "./components/PromptBox"
 
 function App() {
+  const [prompt, setPrompt] = useState(null);
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleOnResponse = async (res) => {
+    console.log("handle on response")
+    if (!res) {
+      console.log("NO response")
+      return;
+    }
+    console.log("response object", res);
+  }
+
+  const handleOnPromptChanged = () => {
+    console.log("prompt changed")
+  }
+
   return (
     <div className="App">
-      <h1>Welcome!</h1>
+      <h1>Prompt Box</h1>
+      <PromptBox onReponse={handleOnResponse} onPromptChanged={handleOnPromptChanged} />
     </div>
   );
 }
