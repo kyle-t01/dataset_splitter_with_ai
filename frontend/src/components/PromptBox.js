a182f1705e1fb7064f1fb6c7dbe58a0c6d8bea37
kylet-01 <userEmail>
2025-03-26 19:15:32 +1100

    added a prompt box where users can type in prompts

---
+++ frontend/src/components/PromptBox.js
@@ -0,0 +1,49 @@
+// component where user queries are sent 
+import { useState } from "react";
+import { fetchAIResponse } from "../services/aiServices";
+
+
+const PromptBox = ({ onResponse, onPromptChanged }) => {
+    const [prompt, setPrompt] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const maxChars = 300;
+
+    const handleSubmitPrompt = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        console.log("handleSubmitPrompt() called");
+        // send user prompt, and get a response
+        // try-catch
+        const response = await fetchAIResponse("null");
+        if (response) {
+            onResponse(response)
+        }
+        console.log(response);
+        setIsLoading(false);
+    }
+    return (
+        <div className="prompt-bar">
+            <textarea
+                className="input"
+                type="text"
+                placeholder="Enter instructions on how data should be split here..."
+
+                value={prompt}
+                // onChange, also do onPromptChanged
+                onChange={(e) => { setPrompt(e.target.value); onPromptChanged(e.target.value) }}
+                rows={3}
+                maxLength={maxChars}
+            />
+            <div className="char-counter">
+                {prompt.length} / {maxChars}
+            </div>
+            <button className="button" onClick={handleSubmitPrompt} disabled={isLoading}>
+                {isLoading ? "A.I. responding..." : "Send"}
+
+            </button>
+        </div>
+    );
+
+}
+
+export default PromptBox;
