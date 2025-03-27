462deb63c846a070c2af68ea76978875825d42b4
kylet-01 <userEmail>
2025-03-27 11:02:31 +1100

    users may now select options for data selector

---
+++ frontend/src/components/DataSelector.js
@@ -0,0 +1,50 @@
+// component where user can select datasets
+import { useState } from "react";
+
+
+
+const DataSelector = ({ onResponse, onPromptChanged }) => {
+    const [option, setOption] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    // max file size in kb
+    const maxFileSize = 300;
+    const options = ["language-flash-cards", "unorganised-to-do-list", "sample-dataset3", "upload your own!"];
+
+    const handleChangeOption = (val) => {
+        setOption(val);
+    }
+
+    const renderOptions = () => {
+        return (
+            <div>
+                {options.map((_, i) => renderOption(i))}
+            </div>
+
+        )
+    }
+
+    const renderOption = (i) => {
+        return (
+            <label>
+                <input
+                    type="radio"
+                    name="dataset"
+                    checked={option == i}
+                    onChange={() => handleChangeOption(i)}
+                />
+                {options[i]}
+            </label>
+        )
+    }
+
+    return (
+        <div className="data-selector">
+            <h1>Select Dataset</h1>
+            {renderOptions()}
+            {/* todo: render file uploader */}
+        </div>
+    );
+}
+
+
+export default DataSelector;
