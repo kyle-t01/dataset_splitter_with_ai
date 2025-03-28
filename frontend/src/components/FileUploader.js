183c7faff0693ee6ba1fbba8960ee2cae07ca92c
kylet-01 <userEmail>
2025-03-28 19:27:22 +1100

    enforce max file size uploaded by user to be 93KB

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -5,10 +5,10 @@ import { GlobalVars } from "../context/GlobalContext";
 
 
 const FileUploader = () => {
-    const { setFile } = GlobalVars()
+    const { file, setFile } = GlobalVars()
     const [isLoading, setIsLoading] = useState(false);
     // max file size in kb
-    const maxFileSize = 300;
+    const maxFileSize = 93;
 
     const handleFileChange = (e) => {
         const selectedFile = e.target.files[0];
@@ -25,12 +25,28 @@ const FileUploader = () => {
 
 
         // file size
+        const fileSizeKB = selectedFile.size / 1024;
+        if (fileSizeKB > maxFileSize) {
+            alert(`File size must not exceed ${maxFileSize}KB!`)
+            e.target.value = null;
+            setFile(null)
+            return;
+        }
 
         // preview
 
         setFile(selectedFile)
         return;
     }
+
+    const renderFileSize = () => {
+        if (!file) return;
+
+        return (
+            <p>File size (must not exceed {maxFileSize} KB): {(file.size / 1024).toFixed(2)} KB</p>
+        );
+    }
+
 
     return (
         <div className="file-uploader">
@@ -40,7 +56,7 @@ const FileUploader = () => {
                 accept=".csv,.tsv,.txt"
                 onChange={handleFileChange}
             />
-
+            {renderFileSize()}
         </div>
     );
 }
